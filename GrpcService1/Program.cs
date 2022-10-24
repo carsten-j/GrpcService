e3e6ab843a3b302ea007b0740bc3f87993d21a11
Carsten Jørgensen <carsten.jorgensen@eksponent.com>
2022-10-24 19:22:00 +0200

    fix bug

--- GrpcService1/Program.cs
+++ GrpcService1/Program.cs
@@ -25,5 +25,5 @@ var app = builder.Build();
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 app.MapGrpcReflectionService();
-}
+
 app.Run();
