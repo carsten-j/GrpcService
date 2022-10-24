3d9002774a4e4aaf3d8b661adedf733d41b50b71
Carsten Jørgensen <carsten.jorgensen@eksponent.com>
2022-10-24 19:28:36 +0200

    remove reflection

--- GrpcService1/Program.cs
+++ GrpcService1/Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
-builder.Services.AddGrpcReflection();
 
 builder.WebHost.ConfigureKestrel(options =>
 {
@@ -24,6 +23,5 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-app.MapGrpcReflectionService();
 
 app.Run();
